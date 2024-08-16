445d6b243c848af63cccec0873be93a5d75a8709
ahmedzubaca <ahmed.zubaca@gmail.com>
2024-08-16 10:06:03 +0200

    izmjena 16.8 druga

--- src/pages/home/components/Home.jsx
+++ src/pages/home/components/Home.jsx
@@ -6,22 +6,25 @@ import styles from '../css_modules/Home.module.css';
 
 function Home() {
   return (
-    <motion.div 
-      initial={{opacity: 0}} 
-      animate={{opacity: 1}}
-      exit={{opacity: 0.5, transition: {duration: 0.1}}}
-      transition={{duration: 0.5}}
-      className={styles.overallContainer}
-    >
-      <div className={styles.textDiv}>          
-        <p className= {styles.textSpanSmall}> Naša garancija je </p>
-        <p className={styles.textSpanCapital}> ROK I KVALITET </p>
-        <p className= {styles.textSpanSmall}> u svemu što radimo </p>            
-      </div>
+    
+    <>
+      <motion.div 
+        initial={{opacity: 0}} 
+        animate={{opacity: 1}}
+        exit={{opacity: 0.5, transition: {duration: 0.1}}}
+        transition={{duration: 0.5}}
+        className={styles.overallContainer}>
+          <div className={styles.textDiv}>          
+              <p className= {styles.textSpanSmall}> Naša garancija je </p>
+              <p className={styles.textSpanCapital}> ROK I KVALITET </p>
+              <p className= {styles.textSpanSmall}> u svemu što radimo </p>            
+        </div>      
+      </motion.div> 
       <Services /> 
       <ReferenceLogos />
       <Footer />
-    </motion.div>   
+    </> 
+    
   )
 } 
 export default Home;
