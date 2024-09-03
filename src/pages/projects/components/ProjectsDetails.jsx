990651c25886748e2e4c3d5e25daa0a149a8ca24
ahmedzubaca <ahmed.zubaca@gmail.com>
2024-09-03 11:11:27 +0200

    background

--- src/pages/projects/components/ProjectsDetails.jsx
+++ src/pages/projects/components/ProjectsDetails.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, useRef } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { TbArrowBadgeLeftFilled, TbArrowBadgeRightFilled } from "react-icons/tb";
-import Projects from './Projects';
 import useWindowResize from '../helperFiles/windowWidth';
 import { imageVideoRender } from '../helperFiles/imageVideoRender';
 import styles from '../cssModules/ProjectsDetails.module.css';
@@ -73,53 +72,52 @@ const ProjectsDetails = () => {
   return (
     <> 
       <div className={styles.backgroundProjects}>
-        <Projects />
-      </div>             
-      <motion.div 
-        initial={{opacity: 0}} 
-        animate={{opacity: 1}}
-        exit={{opacity: 0.5, transition: {duration: 0.1}}}
-        transition={{duration: 0.5}}
-        className={styles.pageContainer}>
-        <div className={styles.buttonContainer}>
-          <button className={styles.backButton}
-              onClick={handleBackButton}> NAZAD
-          </button>
-        </div>
-        <div className={styles.projectCardContainer}>
-          <div className={styles.projectInfo}>
-                <p> Lokacija: {project.location} </p>
-                <p> Investitor: {project.investor} </p>
-                <p> Površina: {project.size}  m<sup>2</sup></p>
+        <motion.div 
+          initial={{opacity: 0}} 
+          animate={{opacity: 1}}
+          exit={{opacity: 0.5, transition: {duration: 0.1}}}
+          transition={{duration: 0.5}}
+          className={styles.pageContainer}>
+          <div className={styles.buttonContainer}>
+            <button className={styles.backButton}
+                onClick={handleBackButton}> NAZAD
+            </button>
           </div>
-          <div className={styles.imageAndArrowsContainer}>
-            <div className={styles.arrowContainer}>
-              <TbArrowBadgeLeftFilled className={`${slideIndex === 0 ? styles.arrowHidden : styles.arrow}`}
-                  size={arrowSize} 
-                  onClick={handlePreviousArrow} />
-            </div>      
-            <div className={styles.imageContainer}> 
-              {                
-                imageVideoRender(imagesLight[slideIndex], imagesNormal[slideIndex], styles.image)                  
-              }      
+          <div className={styles.projectCardContainer}>
+            <div className={styles.projectInfo}>
+                  <p> Lokacija: {project.location} </p>
+                  <p> Investitor: {project.investor} </p>
+                  <p> Površina: {project.size}  m<sup>2</sup></p>
             </div>
-            <div className={styles.arrowContainer}> 
-              <TbArrowBadgeRightFilled className={`${slideIndex === imagesLight.length - 1 ? styles.arrowHidden : styles.arrow}`} 
-                  size={arrowSize} 
-                  onClick={handleNextArrow} />
-            </div>            
+            <div className={styles.imageAndArrowsContainer}>
+              <div className={styles.arrowContainer}>
+                <TbArrowBadgeLeftFilled className={`${slideIndex === 0 ? styles.arrowHidden : styles.arrow}`}
+                    size={arrowSize} 
+                    onClick={handlePreviousArrow} />
+              </div>      
+              <div className={styles.imageContainer}> 
+                {                
+                  imageVideoRender(imagesLight[slideIndex], imagesNormal[slideIndex], styles.image)                  
+                }      
+              </div>
+              <div className={styles.arrowContainer}> 
+                <TbArrowBadgeRightFilled className={`${slideIndex === imagesLight.length - 1 ? styles.arrowHidden : styles.arrow}`} 
+                    size={arrowSize} 
+                    onClick={handleNextArrow} />
+              </div>            
+            </div>
+            <div className={styles.circlesContainer} >
+              {
+                imagesLight.map((slide, index) => (
+                  <span key={index} 
+                      className={`${styles.circle} ${styles.circleMargin} ${slideIndex === index ? styles.activeCircle : ''}`} 
+                      onClick={() => handleCircleClick(index)}> </span>
+                ))
+              } 
+            </div>             
           </div>
-          <div className={styles.circlesContainer} >
-            {
-              imagesLight.map((slide, index) => (
-                <span key={index} 
-                    className={`${styles.circle} ${styles.circleMargin} ${slideIndex === index ? styles.activeCircle : ''}`} 
-                    onClick={() => handleCircleClick(index)}> </span>
-              ))
-            } 
-          </div>             
-        </div>
-      </motion.div>
+        </motion.div>
+      </div>
     </>
   )
 }
